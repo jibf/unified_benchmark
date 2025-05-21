9b7885d6d994be028332cb7278f2b10e9e643748
Eason-Li-AIS <yinsheng.li@mail.mcgill.ca>
2025-05-21 12:10:25 +0800

    Add files via upload

---
+++ evaluation.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import time
+import sys
+from litellm import provider_list
+from methods.evaluate import evaluate
+
+sys.path.append(os.path.abspath('..'))
+os.environ["DEEPINFRA_API_KEY"] = "DEEPINFRA_KEY_REMOVED"
+os.environ["HUGGINGFACE_TOKEN"] = "hf_REMOVED_FROM_HISTORY"
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        type=str,
+        help="The model to use for the agent",
+        default="deepinfra/meta-llama/Llama-3.3-70B-Instruct-Turbo",
+    )
+    parser.add_argument(
+        "--model-provider",
+        type=str,
+        choices=provider_list,
+        help="The model provider for the agent",
+        default="deepinfra",
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=0.0,
+        help="The temperature for the agent",
+    )
+    parser.add_argument("--exp_name", type=str, default=f"default")
+    parser.add_argument("--result_dir", type=str, default="results/")
+    parser.add_argument("--proc_num", type=int, default=16)
+    parser.add_argument("--debug", type=bool, default=False)
+    parser.add_argument(
+        "--task_group",
+        type=str,
+        choices=[
+            "Structured",
+            "Unstructured",
+            "Precise",
+            "Vague",
+            "Completed",
+            "Error",
+            "Single_Object",
+            "Multiple_Objects",
+            "Single_Operation",
+            "Multiple_Operations",
+            "All",
+        ],
+        default="Structured",
+    )
+    parser.add_argument("--huggingface_user_name", type=str, default="Eason666")
+    parser.add_argument("--huggingface_private", type=bool, default=False)
+    parser.add_argument("--vllm_url", type=str, default=None)
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    starttime = time.time()
+    args = parse_args()
+    evaluate(args)
+    endtime = time.time()
+    timespan = endtime - starttime
+    print(f"Benchmark finsihed in {timespan} s.")
+
+
+if __name__ == "__main__":
+    main()
