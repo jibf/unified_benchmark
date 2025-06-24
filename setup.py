10c6f1faf1e9b3aa9563ee2301941b463797f456
Eason-Li-AIS <yinsheng.li@mail.mcgill.ca>
2025-06-24 16:43:41 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="DrafterBench",
+    version="0.1.0",
+    description="A benchmark evaluates LLMs' performance in automating drawing revision tasks.",
+    author="Yinsheng Li, Zhen Dong, Yi Shao",
+    author_email="yinsheng.li@mail.mcgill.ca",
+    packages=find_packages(),
+    python_requires=">=3.10,<3.11",
+    install_requires=requirements,
+)
